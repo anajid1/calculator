68bebf30b14dba540509735060c27677394c4e19
Abdullah Najid <90467466+anajid1@users.noreply.github.com>
2023-01-11 10:43:23 -0500

    Connect frontend and backend of calculator
    * Calculator is functional
    * Decimal decimal doesn't work.

--- calculator.js
+++ calculator.js
@@ -1,24 +1,101 @@
-function add(x, y) { return x + y; }
 
-function subtract(x, y) { return x - y; }
+const calculator = {
+  output: 0,
+  x: 0,
+  y: null,
+  op: null,
 
-function multiply(x, y) { return x * y; }
+  clear() {
+    this.output = 0;
+    this.x = 0;
+    this.y = null;
+    this.op = null;
+  },
 
-function divide(x, y) { return x / y; }
+  setX(x) { this.x = x; }, 
+  setY(y) { this.y = y; },
+  setOp(op) { 
+    this.op = op; 
+    console.log('set op: ' + this.op);
+  },
 
-function operate(operator, x, y) {
-  switch(operator) {
-    case '+':
-      return add(x, y);
-      break;
-    case '-':
-      return subtract(x, y);
-      break;
-    case '*':
-      return multiply(x, y);
-      break;
-    case '/':
-      return division(x, y);
-      break;
+
+  operate() {
+    console.log("x y = : " + this.x + " " + this.y);
+    console.log("op: " + this.op);
+    switch(this.op) {
+      case '+':
+        this.output = this.add();
+        break;
+      case '-':
+        this.output = this.subtract();
+        break;
+      case '*':
+        this.output = this.multiply();
+        break;
+      case '/':
+        this.output = this.divide();
+        break;
+    }
+
+    console.log("OUTPUT: " + this.output);
+    return this.output;
+  },
+
+  add() { return this.x + this.y; },
+  subtract() { return this.x - this.y; },
+  multiply() { return this.x * this.y; },
+  divide() { return this.x / this.y; },
+}
+
+
+/*****************************************************************************/
+
+const number = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ];
+
+const btns = document.getElementsByTagName("button");
+
+for (const btn of btns)
+  btn.onclick = () => handleClick(btn.textContent);
+
+let clearOutput = false;
+
+function handleClick(btnText) {
+  console.log("BTN-TEXT: " + btnText);
+  // Button was a number
+  if (number[btnText]) {
+    if (getOutput() === "0")
+      setOutput(btnText);
+    else if (clearOutput) {
+      setOutput(btnText);
+      clearOutput = false;
+    }
+    else
+      appendOutput(btnText);
+  } else  if (btnText === "clear") {
+    console.log("CLEAR");
+    setOutput(0);
+    calculator.clear();
+    opPressed = false;
+    calculator.clear();
+  } else if (btnText === "=") {
+    const num = parseInt(getOutput());
+    compute(num);
+  } else {
+    clearOutput = true;
+    calculator.setX(parseInt(getOutput()));
+    calculator.setOp(btnText);
   }
 }
+
+function compute(num) {
+  calculator.setY(num);
+  let z = calculator.operate();
+  console.log("z: " + z);
+  setOutput(z);
+}
+
+function getOutput() { return document.getElementById("output").textContent; }
+function setOutput(x) { document.getElementById("output").textContent = x; }
+function appendOutput(x) { document.getElementById("output").textContent += x; }
+
