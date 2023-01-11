3ed45757daaf7324589c1f8ce7d1cd35e506fd27
Abdullah Najid <90467466+anajid1@users.noreply.github.com>
2023-01-11 16:43:58 -0500

    Simplify calculator logic

--- calculator.js
+++ calculator.js
@@ -3,20 +3,20 @@ const calculator = {
   output: 0,
   x: 0,
   y: null,
-  op: null,
+  op: '+',
 
   clear() {
     this.output = 0;
     this.x = 0;
     this.y = null;
-    this.op = null;
+    this.op = '+';
   },
 
   setX(x) { this.x = x; }, 
   setY(y) { this.y = y; },
   setOp(op) { 
     this.op = op; 
-    console.log('set op: ' + this.op);
+    console.log("set op: " + this.op);
   },
 
 
@@ -55,17 +55,23 @@ const number = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ];
 
 const btns = document.getElementsByTagName("button");
 
+console.log("operator: " + calculator.op);
+
 for (const btn of btns)
   btn.onclick = () => handleClick(btn);
 
 let clearOutput = false;
+let newInput = false;
+let equalPressed = false;
 
 function handleClick(btn) {
+  clearButtonColors();
   let btnText = btn.textContent;
   console.log("BTN-TEXT: " + btnText);
   // Button was a number
   if (number[btnText]) {
-    if (getOutput() === "0")
+    newInput = true;
+    if (getOutput() === 0)
       setOutput(btnText);
     else if (clearOutput) {
       setOutput(btnText);
@@ -74,33 +80,64 @@ function handleClick(btn) {
     else
       appendOutput(btnText);
   } else  if (btnText === "clear") {
-    console.log("CLEAR");
     setOutput(0);
+    clearOutput = false;
+    newInput = false;
     calculator.clear();
-    opPressed = false;
-    calculator.clear()
-    clearButtonColors();
-  } else if (btnText === "=") {
-    const num = parseInt(getOutput());
-    compute(num);
-    clearButtonColors();
   } else {
-    clearButtonColors();
     clearOutput = true;
-    btn.style.backgroundColor = "#ACAFBD";
-    calculator.setX(parseInt(getOutput()));
-    calculator.setOp(btnText);
+    compute(btn);
   }
 }
 
-function compute(num) {
-  calculator.setY(num);
+function compute(btn) {
+  const operator = btn.textContent;
+  const num = getOutput();
+
+  if (operator !== '=') {
+    btn.style.backgroundColor = "#ACAFBD";
+  }
+
+  /*
+   Possibilities:
+    newInput | equalPressed | '='
+       0     |     0        |  0   =
+       0     |     0        |  1   = Just do previous operation on the output
+       0     |     1        |  0   = Update x, set new op, exit compute()
+       0     |     1        |  1   =
+       1     |     0        |  0   =
+       1     |     0        |  1   =
+       1     |     1        |  0   =
+       1     |     1        |  1   = 
+   */
+
+  if (newInput && equalPressed) {
+    calculator.setX(num);
+    calculator.setOp(operator);
+    return;
+  }
+  else if (newInput && !equalPressed) {
+    calculator.setY(num);
+    newInput = false;
+  } else if (!newInput && equalPressed && (operator !== '=')) {
+    calculator.setX(getOutput);
+    calculator.setOp(operator);
+    return;
+  }
+
   let z = calculator.operate();
   console.log("z: " + z);
   setOutput(z);
+
+  if (operator !== '=')
+    calculator.setOp(operator);
+  else
+    equalPressed = true;
+
+  calculator.setX(z);
 }
 
-function getOutput() { return document.getElementById("output").textContent; }
+function getOutput() { return parseInt(document.getElementById("output").textContent); }
 function setOutput(x) { document.getElementById("output").textContent = x; }
 function appendOutput(x) { document.getElementById("output").textContent += x; }
 
